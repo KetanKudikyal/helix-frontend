9c8153beb69bf51efbc4300ceb7e0443d3264cf2
Ketan kudikyal <66976099+KetanKudikyal@users.noreply.github.com>
2023-03-01 21:29:25 +0530

    fix: update base_url

--- src/redux/api/index.ts
+++ src/redux/api/index.ts
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery, retry } from '@reduxjs/toolkit/query/react';
-const BASE_URL = 'http://127.0.0.1:8000';
+const BASE_URL = 'https://ketan2.pythonanywhere.com';
 const baseQuery = fetchBaseQuery({
   baseUrl: BASE_URL,
 });
