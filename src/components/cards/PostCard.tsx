ad8da1142dab53e230768d4bc45c8b400f2510e0
Ketan kudikyal <66976099+KetanKudikyal@users.noreply.github.com>
2023-03-02 11:08:58 +0530

    fix: change shimmer color

--- src/components/cards/PostCard.tsx
+++ src/components/cards/PostCard.tsx
@@ -31,18 +31,18 @@ const PostCard: React.FC<
 export const PostCardLoading = () => {
   return (
     <Card className='pb-2'>
-      <div className='mt-2 h-4 w-[70%] animate-pulse rounded-xl bg-slate-200 text-xs font-bold' />
+      <div className='mt-2 h-4 w-[70%] animate-pulse rounded-xl bg-[#ccc] text-xs font-bold' />
       <div className='mb-5 border-b-[1px] border-[#DEDEDE] pt-3 pb-5 text-[15px] font-normal'>
-        <div className='mt-2 h-[12px] w-[90%] animate-pulse rounded-xl bg-slate-200 text-xs font-bold' />
-        <div className='mt-2 h-[12px] w-[80%] animate-pulse rounded-xl bg-slate-200 text-xs font-bold' />
-        <div className='mt-2 h-[12px] w-[60%] animate-pulse rounded-xl bg-slate-200 text-xs font-bold' />
+        <div className='mt-2 h-[12px] w-[90%] animate-pulse rounded-xl bg-[#ccc] text-xs font-bold' />
+        <div className='mt-2 h-[12px] w-[80%] animate-pulse rounded-xl bg-[#ccc] text-xs font-bold' />
+        <div className='mt-2 h-[12px] w-[60%] animate-pulse rounded-xl bg-[#ccc] text-xs font-bold' />
       </div>
 
       <div className='my-3 flex w-full items-start justify-start'>
-        <div className='h-10 w-10 animate-pulse rounded-full bg-slate-200'></div>
+        <div className='h-10 w-10 animate-pulse rounded-full bg-[#ccc]'></div>
         <div className='ml-3'>
-          <div className='h-[14px] w-[150px] animate-pulse rounded-xl bg-slate-200 ' />
-          <div className='mt-2 h-[14px] w-[100px] animate-pulse rounded-xl bg-slate-200 ' />
+          <div className='h-[14px] w-[150px] animate-pulse rounded-xl bg-[#ccc] ' />
+          <div className='mt-2 h-[14px] w-[100px] animate-pulse rounded-xl bg-[#ccc] ' />
         </div>
       </div>
     </Card>
